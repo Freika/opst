4194efb60da9ec9a40f975dce2020b11f34f882c
Freika <iamfrey@gmail.com>
2017-01-03 20:24:18 +0700

    Add bit more statistics to landing page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,10 @@ class PagesController < ApplicationController
   def home
     @matches = Match.count
     @users = User.count
+    @wins = Match.where(result: Match.results[:win]).count
+    @losses = Match.where(result: Match.results[:win]).count
+    @draws = Match.where(result: Match.results[:win]).count
+    @highest_rating = Match.order(skill_rating: :desc).first.skill_rating
 
     render layout: false
   end
