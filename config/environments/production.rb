7fa5edefb7f5824bac3df7d5fabe333b10806141
Freika <iamfrey@gmail.com>
2017-01-04 21:22:57 +0700

    Add host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.default_url_options[:host] = 'http://opst.space'
+  config.action_mailer.default_url_options = { host: 'http://opst.space' }
 end
