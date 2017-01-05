30fcc30ac0f9617be78d50c916824ca5caa9ef18
Freika <iamfrey@gmail.com>
2017-01-05 21:35:04 +0700

    Check if user is admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action do
+    if current_user && current_user.email == 'frey@list.ru'
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
