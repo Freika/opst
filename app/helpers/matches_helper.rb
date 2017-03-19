60e3f8352b11124277a6ab86bf9cb835d2e872a9
Freika <iamfrey@gmail.com>
2017-03-19 18:28:37 +0700

    Revert back.

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -4,12 +4,4 @@ module MatchesHelper
     qualification.persisted? &&
     qualification.skill_rating
   end
-
-  def match_date(user, match)
-    today = match.created_at.beginning_of_day..match.created_at.end_of_day
-
-    if user.matches.where(created_at: today).last ==  match
-      match.created_at.strftime('%d %b')
-    end
-  end
 end
