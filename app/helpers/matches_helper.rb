31b78e43efe0c965be0d53c23ff30ded2ef63963
Freika <iamfrey@gmail.com>
2017-03-19 18:25:53 +0700

    Experiment with date.

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -4,4 +4,12 @@ module MatchesHelper
     qualification.persisted? &&
     qualification.skill_rating
   end
+
+  def match_date(user, match)
+    today = match.created_at.beginning_of_day..match.created_at.end_of_day
+
+    if user.matches.where(created_at: today).last ==  match
+      match.created_at.strftime('%d %b')
+    end
+  end
 end
